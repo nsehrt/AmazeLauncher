97033272ad3f42c543c049c25afaaeb3a1c87ecb
Nicolai Sehrt <nicolai.sehrt@mni.thm.de>
2021-02-07 23:29:54 +0100

    minor

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -333,7 +333,7 @@ namespace AmazeLauncher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 473);
+            this.ClientSize = new System.Drawing.Size(421, 473);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
